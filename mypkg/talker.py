1ba41ddd52ac3f51e860aa4cbc7e610b49ba2449
Yuuya Tanaka <s23c1086uu@s.chibakoudai.jp>
2025-01-01 22:56:41 +0900

    add

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,20 +1,26 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import Float64
+import random
 
 class Talker(Node):
     def __init__(self):
         super().__init__("talker")
-        self.pub = self.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        self.create_timer(0.5, self.cb)
-
+        self.pub = self.create_publisher(Float64, "countup", 10)
+        self.n = 10000.0
+        self.add_initial_value = False
+        self.create_timer(5, self.cb)
 
     def cb(self):
-        msg = Int16()
-        msg.data = self.n
+        msg = Float64()
+        if not self.add_initial_value:
+            self.n += 0.05
+            self.add_initial_value = True
+        else:
+            self.n += random.uniform(-0.9, 0.9)
+
+        msg.data = round(self.n, 2)
         self.pub.publish(msg)
-        self.n += 1
 
 
 def main():
