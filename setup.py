a87b36dc485e4f814eb9ab351d888d68bb601edf
Yuuya Tanaka <s23c1086uu@s.chibakoudai.jp>
2025-01-04 02:29:43 +0900

    add

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'console_scripts': [
             'talker = mypkg.talker:main',
             'listener = mypkg.listener:main',
+            'kabu = mypkg.kabu:main',
 	],
     },
 )
